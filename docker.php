05c5d33463cc7558e53256b5b755ebf53be23491
beans-are-gross <94317146+beans-are-gross@users.noreply.github.com>
2025-03-02 16:14:16 -0600

    Add files via upload

---
+++ docker.php
@@ -0,0 +1,10 @@
+<?php
+//install packages
+echo "Install packages.\n";
+shell_exec("apt-get update");
+shell_exec("apt-get install mysql-server git php-fpm php-mysql -y");
+
+//install mdash
+shell_exec("mkdir /mdash-installer/");
+shell_exec("cd /mdash-installer/ && git clone https://github.com/beans-are-gross/mdash");
+shell_exec("cd /mdash-installer/mdash/ && php setup.php db_host=172.220.0.5 db_pass={$_ENV["DB_PASS"]}");
