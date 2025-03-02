d6cfd17a246cd92612e69f91db8612295745c820
beans-are-gross <94317146+beans-are-gross@users.noreply.github.com>
2025-03-02 16:19:58 -0600

    Added useful debugging info

--- docker.php
+++ docker.php
@@ -1,11 +1,13 @@
 <?php
 //install packages
-echo "Install packages.\n";
+echo "Installing packages.\n";
 shell_exec("apt-get update");
 shell_exec("apt-get install git php-fpm php-mysql -y");
 
 //install mdash
-echo "Install mDash.\n";
+echo "Downloading mDash.\n";
 shell_exec("mkdir /mdash-installer/");
+
+echo "Running mDash install script.\n";
 shell_exec("cd /mdash-installer/ && git clone https://github.com/beans-are-gross/mdash");
-shell_exec("cd /mdash-installer/mdash/ && php setup.php db_host=172.220.0.5 db_pass={$_ENV["DB_PASS"]}");
+shell_exec("cd /mdash-installer/mdash/ && php setup.php db_host=172.220.0.5 db_pass={$_ENV["DB_PASS"]} docker=true");
