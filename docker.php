2eda6fe490f34fc29390e96363e9a14c760d00f0
beans-are-gross <94317146+beans-are-gross@users.noreply.github.com>
2025-03-02 16:47:21 -0600

    Updated to use environment varibales for DB_PASS

--- docker.php
+++ docker.php
@@ -8,6 +8,20 @@ shell_exec("apt-get install git php-fpm php-mysql -y");
 echo "Downloading mDash.\n";
 shell_exec("mkdir /mdash-installer/");
 
+echo "Get environment varibales.\n";
+$env = shell_exec("env");
+$env = explode("\n", $env);
+
+foreach($env as $envVar){
+    $envVar = explode("=", $envVar);
+    if($envVar[0] !== "DB_PASS") continue;
+    else $dbPass = $envVar[1];
+}
+
+if(!isset($dbPass)){
+    die("DB_PASS not found.");
+}
+
 echo "Running mDash install script.\n";
 shell_exec("cd /mdash-installer/ && git clone https://github.com/beans-are-gross/mdash");
-shell_exec("cd /mdash-installer/mdash/ && php setup.php db_host=172.220.0.5 db_pass={$_ENV["DB_PASS"]} docker=true");
+shell_exec("cd /mdash-installer/mdash/ && php setup.php db_host=172.220.0.5 db_pass=$dbPass docker=true");
