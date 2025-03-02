71ac9f2816c8ccaef232f77de0093595db3c7157
beans-are-gross <94317146+beans-are-gross@users.noreply.github.com>
2025-03-02 16:15:09 -0600

    Add install info for debugging and remove mysql-server

--- docker.php
+++ docker.php
@@ -2,9 +2,10 @@
 //install packages
 echo "Install packages.\n";
 shell_exec("apt-get update");
-shell_exec("apt-get install mysql-server git php-fpm php-mysql -y");
+shell_exec("apt-get install git php-fpm php-mysql -y");
 
 //install mdash
+echo "Install mDash.\n";
 shell_exec("mkdir /mdash-installer/");
 shell_exec("cd /mdash-installer/ && git clone https://github.com/beans-are-gross/mdash");
 shell_exec("cd /mdash-installer/mdash/ && php setup.php db_host=172.220.0.5 db_pass={$_ENV["DB_PASS"]}");
