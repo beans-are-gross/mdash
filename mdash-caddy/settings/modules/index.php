a75f8996dbd9ea1f47f39100e728a9e4ef6b3430
beans-are-gross <94317146+beans-are-gross@users.noreply.github.com>
2025-03-07 15:31:27 -0600

    Update the background image, add a filter for modules, update the read me

--- mdash-caddy/settings/modules/index.php
+++ mdash-caddy/settings/modules/index.php
@@ -12,8 +12,12 @@ require_once "/var/www/mdash/header.php";
             <h1>Modules</h1>
             <p id="stats" class="secondary"></p>
             <div class="center">
-                <button onclick="history.go(-1);">Back </button>
+                <button onclick="window.location.href= '../';">Back </button>
                 <button onclick="window.location.href = './add'">Add a Module</button>
+                <select id="filter-select" onchange="window.location.href = './?filter=' + this.value;">
+                    <option value="all">All</option>
+                    <option value="skip" id="skip">Non-Standard</option>
+                </select>
             </div>
             <table>
                 <thead>
@@ -22,7 +26,16 @@ require_once "/var/www/mdash/header.php";
                 </thead>
                 <tbody>
                     <?php
-                    $modules = shell_exec("caddy list-modules --skip-standard --versions");
+                    if (isset($_GET["filter"])) {
+                        if ($_GET["filter"] == "skip") {
+                            $modules = shell_exec("caddy list-modules --skip-standard --versions");
+                            echo "<script>document.getElementById('skip').selected = true;</script>";
+                        } else {
+                            $modules = shell_exec("caddy list-modules --versions");
+                        }
+                    } else {
+                        $modules = shell_exec("caddy list-modules --versions");
+                    }
                     $modules = explode("\n", $modules);
                     $stats = "";
                     foreach ($modules as $module) {
